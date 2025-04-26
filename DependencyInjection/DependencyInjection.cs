a59ad50d08a7f5042674f2f1977ee181759b2c0d
Eren Gundemir <69444504+c0xad@users.noreply.github.com>
2025-04-26 23:05:29 +0300

    Add files via upload

--- DependencyInjection/DependencyInjection.cs
+++ DependencyInjection/DependencyInjection.cs
@@ -1,16 +1,26 @@
-﻿using MediatR;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using SupportRequestManagement.Domain.Interfaces;
+using SupportRequestManagement.Infrastructure.Data; // Repository'ler için
+using SupportRequestManagement.Infrastructure.Services; // Servisler için
+using System.Data;
 
-
-namespace SupportRequestManagement.Core.Application.DependencyInjection
+namespace SupportRequestManagement.Infrastructure.DependencyInjection
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-            services.AddAutoMapper(typeof(Mapping.MappingProfile));
+            // PostgreSQL bağlantısı
+            services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(configuration.GetConnectionString("DefaultConnection")));
+
+            // Repository'ler gelecek
+         
+
+            // Servisler gelecek 
+        
+
             return services;
         }
     }
